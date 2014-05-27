9debc32667a79e92d6e32b0f9f8dcc244b5cb1df
Kelsey Woodard <kwoodard22@gmail.com>
2014-05-27 12:40:12 -0400

    added digg and reddit

--- Kelsey_Woodard/hw2_ex_teddit_api_news.rb
+++ Kelsey_Woodard/hw2_ex_teddit_api_news.rb
@@ -12,23 +12,52 @@ require 'json'
 
 def get_mashable_stories # Pulls the json and parses story
 	response = JSON.parse(RestClient.get 'http://mashable.com/stories.json')
-	news_array = Array.new
+	news_array = []
 	response['new'].each do |story|
 		title = story['title']
-		category = story['channel']
+		category = story['channel'].downcase
 		upvotes = calculate_upvotes(title, category) # Calls calculate_upvotes method
 		hash = { :title => "#{title}", :category => "#{category}", :upvotes => "#{upvotes}" }
 		news_array << hash # Inserts hashes into the array
 	end
+	puts "========== Mashable Stories =========="	
+	puts ''
 	front_page(news_array)
 end
 
+def get_digg_stories # Pulls the json and parses story
+	response = JSON.parse(RestClient.get 'http://digg.com/api/news/popular.json')
+	news_array = []
+	response['data']['feed'].each do |story|
+		title = story['content']['title']
+		category = story['content']['tags'][0]['name'].downcase
+		upvotes = calculate_upvotes(title, category) # Calls calculate_upvotes method
+		hash = { :title => "#{title}", :category => "#{category}", :upvotes => "#{upvotes}" }
+		news_array << hash # Inserts hashes into the array
+	end
+	puts "========== Digg Stories =========="
+	puts ''
+	front_page(news_array)
+end
+
+def get_reddit_stories # Pulls the json and parses story
+	response = JSON.parse(RestClient.get 'http://www.reddit.com/.json')
+	news_array = []
+	response['data']['children'].each do |story|
+		title = story['data']['title']
+		category = story['data']['subreddit'].downcase
+		upvotes = calculate_upvotes(title, category) # Calls calculate_upvotes method
+		hash = { :title => "#{title}", :category => "#{category}", :upvotes => "#{upvotes}" }
+		news_array << hash # Inserts hashes into the array
+	end
+	puts "========== Reddit Stories =========="
+	puts ''
+	front_page(news_array)
+end
 
 def front_page(news_array) # Displays front page
-	puts "Welcome to Teddit! a text based news aggregator. Get today's news tomorrow!"
-	puts ""
 	news_array.each do |story| 
-		puts "New story added! #{story[:title]}"
+		puts "#{story[:title]}"
 		puts "Category: #{story[:category]}"
 		puts "Current Upvotes: #{story [:upvotes]}"
 		puts ""
@@ -38,22 +67,24 @@ end
 
 def calculate_upvotes(title, category) # Calculates upvotes
 	upvotes = 1
-	if category.include?('Lifestyle') || title.include?('Lifestyle')
+	if category.include?('food') || title.include?('food')
 		upvotes *= 5 
 	end
 
-	if category.include?('Tech') || title.include?('Tech')
+	if category.include?('tech') || title.include?('tech')
 		upvotes *= 8 
 	end
 
-	if category.include?('Entertainment') || title.include?('Entertainment')
+	if category.include?('business') || title.include?('business')
 		upvotes *= 3 
 	end
 
 	upvotes
 end
 
-
+puts ''
+puts "Welcome to Teddit! a text based news aggregator. Get today's news tomorrow!"
+puts ''
 get_mashable_stories
-front_page(get_mashable_stories)
-
+get_digg_stories
+get_reddit_stories
